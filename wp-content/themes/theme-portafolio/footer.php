73322f58a8b10d6862a2c70e10a67ff1ee752cf5
Juan Pablo Contreras Valdés <juanpablo@wanna.bi>
2014-12-19 15:15:00 -0300

    19121514

---
+++ wp-content/themes/theme-portafolio/footer.php
@@ -0,0 +1,8 @@
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
+    <?php wp_footer(); ?>
+  </body>
+</html>
---
+++ wp-content/themes/theme-portafolio/index.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+  
+    <div class="container-fluid">
+        <div id="menu" class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+
+            <?php include 'nav.php'; ?>
+            
+        </div>
+        
+        
+        <div id="principal" class="col-lg-6 col-md-6 col-sm-10 col-xs-12">
+            <div class="contenido box fleft fwidth text-justified">
+                <figure>
+                    <img src="<?php bloginfo('template_url'); ?>/img/perfil.jpg" alt="Yo, 1989">
+                    <figcaption>Yo, 1987.</figcaption>
+                </figure>
+                <p>Hola, mi nombre es Juan Pablo Contreras Valdés.
+Tengo 27 años y nací en Santiago, Chile.
+
+En los últimos 3 años de mi vida me he dedicado a diseñar, programar en HTML/CSS (HTML 4, 5, CSS 3, any color you like) (integrar librerías Javascript también) y aprender todo lo que WordPress pueda enseñarme (me gusta mucho WordPress, tanto que realicé un taller gratuito para enseñar a quienes no lo conocen aún).
+Ser autodidacta ha sido el camino, aunque estudié 3 años en la Universidad de Valparaíso. También hice un semestre de Sociología en la Universidad de Buenos Aires y otro semestre de Diseño, también en la Universidad de Buenos Aires.
+
+Creo en el trabajo en equipo y que no hay pregunta tonta sino tonto que no pregunta. Manejo el idioma inglés de forma fluida, tanto oral como escrito.
+
+Tengo varios proyectos personales andando, comenzaré desde atrás hasta la actualidad: El primero es una empresa de hosting con el maestro Christian Aguilera, llamada Dommushost. En segundo lugar, Ante, un proyecto de Venture Branding con el cual trabajamos con marcas que realicen emprendimientos y nos sumamos a su proyecto a cambio de beneficios a posteriori. También participo en Imetricmedia, proyecto en el cual cumplo el rol de director de proyectos y wizard de la programación. Me gusta emprender, apostar a ideas nuevas y trabajar en equipo (creo que mencioné eso antes).
+
+Me apasionan las estadísticas, todo lo que tenga que ver con medición. Actualmente asesoro a sitios (El Fracaso y Chilenos en Argentina) en sus datos de métricas y monetización. Les ayudo a hacer seguimiento de sus métricas y les voy haciendo reportes más amigables y más fáciles de interpretar.
+
+Mi interés al momento de desarrollar procesos que concluyen en un interfaz es considerar procedimientos previos.
+
+Hablando de mis aficiones, diría que me gusta mucho andar en bici. También pongo visuales en tocatas, para amigos
+
+Llegando a la parte técnica, he desarrollado web para móviles con responsive design, manejo el HTML5 y CSS3, manejo WordPress y sus funciones, también tengo experiencia usando programas de post-producción como After Effects, Adobe Premiere y Soundbooth.
+
+También tengo una cuenta de Flickr, una de Facebook y otra de Twitter. </p>
+            </div>
+        </div>  
+    </div>
+  
+
+<?php get_footer(); ?>
