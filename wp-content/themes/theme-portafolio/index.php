cef9f679b38cb29104f7c4a106f9c409eaa1fdb6
Juan Pablo Contreras Valdés <juanpablo@wanna.bi>
2014-12-19 15:35:49 -0300

    19121535

--- wp-content/themes/theme-portafolio/index.php
+++ wp-content/themes/theme-portafolio/index.php
@@ -15,24 +15,19 @@
                     <figcaption>Yo, 1987.</figcaption>
                 </figure>
                 <p>Hola, mi nombre es Juan Pablo Contreras Valdés.
-Tengo 27 años y nací en Santiago, Chile.
+Tengo 30 años y nací en Santiago, Chile.</p>
 
-En los últimos 3 años de mi vida me he dedicado a diseñar, programar en HTML/CSS (HTML 4, 5, CSS 3, any color you like) (integrar librerías Javascript también) y aprender todo lo que WordPress pueda enseñarme (me gusta mucho WordPress, tanto que realicé un taller gratuito para enseñar a quienes no lo conocen aún).
-Ser autodidacta ha sido el camino, aunque estudié 3 años en la Universidad de Valparaíso. También hice un semestre de Sociología en la Universidad de Buenos Aires y otro semestre de Diseño, también en la Universidad de Buenos Aires.
+<p>En los últimos 4-5 años de mi vida me he dedicado a diseñar, programar en HTML/CSS/Javascript y aprender todo lo que WordPress pueda enseñarme (me gusta mucho WordPress, tanto que realicé un taller gratuito para enseñar a quienes no lo conocen aún).</p>
+<p>Ser autodidacta ha sido el camino, aunque estudié 3 años en la Universidad de Valparaíso. También hice un semestre de Sociología en la Universidad de Buenos Aires y otro semestre de Diseño, también en la Universidad de Buenos Aires.</p>
 
-Creo en el trabajo en equipo y que no hay pregunta tonta sino tonto que no pregunta. Manejo el idioma inglés de forma fluida, tanto oral como escrito.
+<p>Creo en el trabajo en equipo y que no hay pregunta tonta sino tonto que no pregunta. Manejo el idioma inglés de forma fluida, tanto oral como escrito.</p>
 
-Tengo varios proyectos personales andando, comenzaré desde atrás hasta la actualidad: El primero es una empresa de hosting con el maestro Christian Aguilera, llamada Dommushost. En segundo lugar, Ante, un proyecto de Venture Branding con el cual trabajamos con marcas que realicen emprendimientos y nos sumamos a su proyecto a cambio de beneficios a posteriori. También participo en Imetricmedia, proyecto en el cual cumplo el rol de director de proyectos y wizard de la programación. Me gusta emprender, apostar a ideas nuevas y trabajar en equipo (creo que mencioné eso antes).
+<p>Mi interés al momento de desarrollar procesos que concluyen en un interfaz es considerar procedimientos previos.</p>
 
-Me apasionan las estadísticas, todo lo que tenga que ver con medición. Actualmente asesoro a sitios (El Fracaso y Chilenos en Argentina) en sus datos de métricas y monetización. Les ayudo a hacer seguimiento de sus métricas y les voy haciendo reportes más amigables y más fáciles de interpretar.
+<p>Hablando de mis aficiones, diría que me gusta mucho andar en bici.</p>
 
-Mi interés al momento de desarrollar procesos que concluyen en un interfaz es considerar procedimientos previos.
+                <p>Llegando a la parte técnica, he desarrollado web para móviles con responsive design, manejo el HTML5, CSS3, SASS, WooCommerce, obviamente WordPress y sus funciones, también tengo experiencia usando programas de post-producción como After Effects, Adobe Premiere y Soundbooth.</p>
 
-Hablando de mis aficiones, diría que me gusta mucho andar en bici. También pongo visuales en tocatas, para amigos
-
-Llegando a la parte técnica, he desarrollado web para móviles con responsive design, manejo el HTML5 y CSS3, manejo WordPress y sus funciones, también tengo experiencia usando programas de post-producción como After Effects, Adobe Premiere y Soundbooth.
-
-También tengo una cuenta de Flickr, una de Facebook y otra de Twitter. </p>
             </div>
         </div>  
     </div>
--- wp-content/themes/theme-portafolio/page-contacto.php
+++ wp-content/themes/theme-portafolio/page-contacto.php
@@ -7,6 +7,7 @@
         </div>
         <div id="principal" class="col-lg-6 col-md-6 col-sm-10 col-xs-12">
             <div class="contenido box fleft fwidth">
+                <?php echo do_shortcode('[contact-form-7 id="14" title="Contacto"]'); ?>
                 
             </div>
         </div>  
