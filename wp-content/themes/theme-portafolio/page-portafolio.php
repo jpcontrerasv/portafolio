219233b38a680b3063061a52b36efdf4d3e60c79
Juan Pablo Contreras Valdés <juanpablo@wanna.bi>
2014-12-19 15:57:35 -0300

    19121557

--- wp-content/themes/theme-portafolio/page-portafolio.php
+++ wp-content/themes/theme-portafolio/page-portafolio.php
@@ -11,55 +11,55 @@
                 
                 <ul>
                     <li>
-                        <h4><a href="#" target="_blank">Sebastián Jiménez</a></h4>
+                        <h4><a href="http://201.238.235.41/clientes/sebastian-jimenez/" target="_blank">Sebastián Jiménez *</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Revista El Fracaso</a></h4>
+                        <h4><a href="http://www.elfracaso.cl" target="_blank">Revista El Fracaso</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Baik</a></h4>
+                        <h4><a href="http://www.baik.cl" target="_blank">Baik</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">CAAF</a></h4>
+                        <h4><a href="http://www.caaf.cl" target="_blank">CAAF</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Flyersurf</a></h4>
+                        <h4><a href="http://www.flyersurf.cl/" target="_blank">Flyersurf</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Teka</a></h4>
+                        <h4><a href="http://201.238.235.41/clientes/teka/home.php" target="_blank">Teka</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Visto Bueno</a></h4>
+                        <h4><a href="http://201.238.235.41/clientes/visto-bueno/titulo/" target="_blank">Visto Bueno</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Perdí el Carnet</a></h4>
+                        <h4><a href="http://www.perdielcarnet.cl/" target="_blank">Perdí el Carnet</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">The Mine</a></h4>
+                        <h4><a href="http://www.themine.cl/" target="_blank">The Mine</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Subterra</a></h4>
+                        <h4><a href="http://www.subterramineria.cl/" target="_blank">Subterra</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Novasur - La TV que quiero ver</a></h4>
+                        <h4><a href="http://www.novasur.cl/latvquequierover/" target="_blank">Novasur - La TV que quiero ver</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Juan Pablo Molina</a></h4>
+                        <h4><a href="http://www.juanpablomolina.net/" target="_blank">Juan Pablo Molina</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Hellowine</a></h4>
+                        <h4><a href="http://www.hellowine.cl/" target="_blank">Hellowine</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Andrés Tapia</a></h4>
+                        <h4><a href="http://www.andrestapia.cl/" target="_blank">Andrés Tapia</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Penta</a></h4>
+                        <h4><a href="http://201.238.235.41/clientes/penta/nuevaweb/" target="_blank">Penta</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">MUI</a></h4>
+                        <h4><a href="http://prueba.mui.cl/" target="_blank">MUI</a></h4>
                     </li>
                     <li>
-                        <h4><a href="#" target="_blank">Mañana no hay Mañana</a></h4>
+                        <h4><a href="http://201.238.235.41/clientes/mnhm" target="_blank">Mañana no hay Mañana</a></h4>
                     </li>
                     
                 </ul>
